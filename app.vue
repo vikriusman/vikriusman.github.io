2765ea5211d46a0a30f44c1644e5c946cfaa8daa
kintanr <kintanumari178@gmail.com>
2025-05-14 15:12:24 +0700

    add footer

--- app.vue
+++ app.vue
@@ -153,7 +153,36 @@
       </section>
     </div>
 
-    <div class="border-t-2 border-zinc-400 bg-zinc-800 md:p-25 p-10"></div>
+    <div
+      class="border-t-2 border-zinc-300 bg-zinc-200 dark:bg-zinc-800 md:px-25 md:py-5 py-5 px-10 justify-between h-full"
+    >
+      <div class="grid grid-cols-2 gap-4 items-center">
+        <div class="col-span-2 md:col-span-1">
+          <div>Oniel, Lily adn Azizi</div>
+          <div>working on JKT48</div>
+        </div>
+        <div class="col-span-2 md:col-span-1 justify-items-end">
+          <div class="font-bold mb-2">Socials Media</div>
+          <div class="flex gap-3 flex-wrap">
+            <a
+              v-for="(data, key) in socials"
+              :key="key"
+              :href="
+                containsHttps(data.link) ? data.link : 'mailto:' + data.link
+              "
+              target="_blank"
+            >
+              <img :src="data.icon" width="25" class="hidden dark:block" />
+              <img :src="data.dark_icon" width="25" class="block dark:hidden" />
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="text-center mt-10 dark:text-gray-400 text-gray-600">
+        &copy; Copyright {{ year }}. Made by Kintan Umari
+      </div>
+    </div>
   </div>
 </template>
 
@@ -168,6 +197,10 @@ import shape from "~/assets/images/shape.png";
 import gmail from "~/assets/images/gmail.png";
 import gmail_dark from "~/assets/images/gmail-dark.png";
 import skill from "~/assets/images/skills.png";
+
+import skills from "~/data/skills.json";
+
+const year = new Date().getFullYear();
 
 const socials = [
   {
@@ -217,29 +250,6 @@ const projects = [
     sub_title: "Web Development",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ullamco",
-  },
-];
-
-const skills = [
-  {
-    title: "skill title",
-    item: ["html", "js", "abacadabra"],
-  },
-  {
-    title: "skill title",
-    item: ["html", "js", "abacadabra"],
-  },
-  {
-    title: "skill title",
-    item: ["html", "js", "abacadabra"],
-  },
-  {
-    title: "skill title",
-    item: ["html", "js", "abacadabra"],
-  },
-  {
-    title: "skill title",
-    item: ["html", "js", "abacadabra"],
   },
 ];
 
